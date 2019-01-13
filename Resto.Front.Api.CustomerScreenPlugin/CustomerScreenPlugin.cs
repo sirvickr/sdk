46bfb56d7ea87dca7da97822f982afd85df0c3a7
sirvickr <sirvickr@gmail.com>
2019-01-13 14:31:12 +0300

    [+] CustomerScreenPlugin

---
+++ Resto.Front.Api.CustomerScreenPlugin/CustomerScreenPlugin.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Globalization;
+using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Windows;
+using Resto.Front.Api.CustomerScreen.Settings;
+using Resto.Front.Api.V5.Attributes;
+using Resto.Front.Api.V5.Data.Organization;
+using Resto.Front.Api.CustomerScreen.Helpers;
+using Resto.Front.Api.CustomerScreen.View;
+using Resto.Front.Api.CustomerScreen.ViewModel;
+using Resto.Front.Api.V5;
+using Resto.Front.Api.V5.Attributes.JetBrains;
+using Resto.Front.Api.V5.Data.Screens;
+
+namespace Resto.Front.Api.CustomerScreen
+{
+    [UsedImplicitly,PluginLicenseModuleId(ModuleId)]
+    public sealed class CustomerScreenPlugin : IFrontPlugin
+    {
+        private const int ModuleId = 10000;
+        private readonly CompositeDisposable unsubscribe = new CompositeDisposable();
+        private static Order vmOrder;
+        private static CustomerScreenWindow customerScreen;
+        public static ICurrencySettings CurrencySettings;
+
+        public CustomerScreenPlugin()
+        {
+            var screenHelper = new ScreenHelper();
+            if (!screenHelper.IsSecondMonitorExists)
+            {
+                PluginContext.Log.Info("Can't show customer screen. There is no second monitor.");
+                return;
+            }
+
+            try
+            {
+                CustomerScreenConfig.Init(PluginContext.Integration.GetConfigsDirectoryPath());
+                InitializeUiDispatcher(PluginContext.Operations.GetHostTerminalCultureInfo());
+                CurrencySettings = PluginContext.Operations.GetHostRestaurant().Currency;
+
+                Application.Current.Dispatcher.Invoke(new Action(() =>
+                {
+                    vmOrder = new Order();
+
+                    unsubscribe.Add(PluginContext.Notifications.ScreenChanged.ObserveOn(DispatcherScheduler.Current).Subscribe(OnScreenChanged));
+                    unsubscribe.Add(PluginContext.Notifications.ChangeSumChanged.ObserveOn(DispatcherScheduler.Current).Subscribe(OnChangeSumChanged));
+                    unsubscribe.Add(PluginContext.Notifications.OrderChanged.ObserveOn(DispatcherScheduler.Current).Where(o => Equals(vmOrder.OrderSource, o)).Subscribe(vmOrder.Update));
+                    unsubscribe.Add(PluginContext.Notifications.RestaurantChanged.ObserveOn(DispatcherScheduler.Current).Subscribe(r => OnRestaurantChanged(r.Currency)));
+
+                    ShowCustomerScreen(screenHelper);
+                }));
+            }
+            catch
+            {
+                Dispose();
+                throw;
+            }
+        }
+
+        private static void ShowCustomerScreen(ScreenHelper screenHelper)
+        {
+            customerScreen = new CustomerScreenWindow
+            {
+                Top = screenHelper.SecondMonitorTop,
+                Left = screenHelper.SecondMonitorLeft,
+                DataContext = vmOrder
+            };
+            PluginContext.Log.Info("Customer screen was created.");
+            customerScreen.Show();
+            PluginContext.Log.Info("Customer screen was shown.");
+
+            customerScreen.WindowState = WindowState.Maximized;
+            customerScreen.mediaControl.StartPlayer();
+        }
+
+        private static void OnScreenChanged(IScreen screen)
+        {
+            PluginContext.Log.Info(screen.ToString());
+            var swo = screen as IScreenWithOrder;
+            if (swo != null)
+            {
+                PluginContext.Log.Info(swo.Order.Number.ToString());
+                vmOrder.Update(swo.Order);
+            }
+            else
+                vmOrder.Reset();
+        }
+
+        private static void OnChangeSumChanged(decimal sum)
+        {
+            customerScreen.ChangeSumChanged(sum);
+        }
+
+        private static void OnRestaurantChanged(ICurrencySettings currencySettings)
+        {
+            CurrencySettings = currencySettings;
+        }
+
+        public void Dispose()
+        {
+            ShutdownUiDispatcher();
+            unsubscribe.Dispose();
+        }
+
+        private static void InitializeUiDispatcher(CultureInfo uiCulture)
+        {
+            using (var uiThreadStartedSignal = new ManualResetEvent(false))
+            {
+                var thread = new Thread(() =>
+                {
+                    var app = new Application { ShutdownMode = ShutdownMode.OnExplicitShutdown };
+                    StartupEventHandler startupHandler = null;
+                    startupHandler = (s, e) =>
+                    {
+                        // ReSharper disable once AccessToDisposedClosure (calling thread wouldn't exit from 'using' until we call 'Set')
+                        uiThreadStartedSignal.Set();
+                        app.Startup -= startupHandler;
+                    };
+                    app.Startup += startupHandler;
+                    app.Run();
+                });
+                thread.CurrentUICulture = uiCulture;
+                thread.SetApartmentState(ApartmentState.STA);
+                thread.Start();
+
+                uiThreadStartedSignal.WaitOne();
+            }
+        }
+
+        private static void ShutdownUiDispatcher()
+        {
+            if (Application.Current == null)
+                return;
+
+            Application.Current.Dispatcher.InvokeShutdown();
+            Application.Current.Dispatcher.Thread.Join();
+        }
+    }
+}
---
+++ Resto.Front.Api.CustomerScreenPlugin/Resources/CustomerScreenLocalResources.generated.cs
@@ -0,0 +1,165 @@
+﻿// This file was generated with XmlToCodeGenerator.
+// Do not edit it manually.
+
+
+namespace Resto.Front.Localization.Resources
+{
+    public class CustomerScreenLocalResources
+    {
+        private static readonly Resto.Framework.Localization.Localizer Localizer = Resto.Framework.Localization.Localizer.Create("CustomerScreenLocalResources.resx", "Resto.Front.Api.CustomerScreen.Resources.CustomerScreenLocalResources", System.Reflection.Assembly.GetExecutingAssembly());
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Итого: "
+        /// </summary>
+        public static System.String TotalSumText
+        {
+            get { return Localizer.GetStringFromResources("TotalSumText"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Скидка: "
+        /// </summary>
+        public static System.String DiscountText
+        {
+            get { return Localizer.GetStringFromResources("DiscountText"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Надбавка: "
+        /// </summary>
+        public static System.String IncreaseText
+        {
+            get { return Localizer.GetStringFromResources("IncreaseText"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Содержание заказа:"
+        /// </summary>
+        public static System.String OrderContent
+        {
+            get { return Localizer.GetStringFromResources("OrderContent"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Окно покупателя"
+        /// </summary>
+        public static System.String WindowTitle
+        {
+            get { return Localizer.GetStringFromResources("WindowTitle"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Сдача: "
+        /// </summary>
+        public static System.String ChangeSumText
+        {
+            get { return Localizer.GetStringFromResources("ChangeSumText"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Добро пожаловать!"
+        /// </summary>
+        public static System.String WelcomeTextDefault
+        {
+            get { return Localizer.GetStringFromResources("WelcomeTextDefault"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "По указанному пути не найдено ни одного подходящего для воспроизведения видеофайла. Удостоверьтесь, что файлы есть и перезапустите iikoFront."
+        /// </summary>
+        public static System.String FilesNotFoundErrorText
+        {
+            get { return Localizer.GetStringFromResources("FilesNotFoundErrorText"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Наименование"
+        /// </summary>
+        public static System.String ProductName
+        {
+            get { return Localizer.GetStringFromResources("ProductName"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "кол-во"
+        /// </summary>
+        public static System.String Amount
+        {
+            get { return Localizer.GetStringFromResources("Amount"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "стоимость"
+        /// </summary>
+        public static System.String Price
+        {
+            get { return Localizer.GetStringFromResources("Price"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Сумма заказа:"
+        /// </summary>
+        public static System.String OrderSum
+        {
+            get { return Localizer.GetStringFromResources("OrderSum"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Экран покупателя"
+        /// </summary>
+        public static System.String PluginName
+        {
+            get { return Localizer.GetStringFromResources("PluginName"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Операции"
+        /// </summary>
+        public static System.String ConfigOperations
+        {
+            get { return Localizer.GetStringFromResources("ConfigOperations"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Восстано­вить окно"
+        /// </summary>
+        public static System.String ConfigValidareWindow
+        {
+            get { return Localizer.GetStringFromResources("ConfigValidareWindow"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Пере­запус­тить видео"
+        /// </summary>
+        public static System.String ConfigRestartPlayer
+        {
+            get { return Localizer.GetStringFromResources("ConfigRestartPlayer"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "В системе отсутствует второй монитор."
+        /// </summary>
+        public static System.String CantFindSecondScreenError
+        {
+            get { return Localizer.GetStringFromResources("CantFindSecondScreenError"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Информация"
+        /// </summary>
+        public static System.String InfoMessageTitle
+        {
+            get { return Localizer.GetStringFromResources("InfoMessageTitle"); }
+        }
+
+        /// <summary>
+        ///   Looks up a localized string similar to "Экран покупателя был повторно проинициализирован, проверьте результат."
+        /// </summary>
+        public static System.String CusromerScreenShowSuccess
+        {
+            get { return Localizer.GetStringFromResources("CusromerScreenShowSuccess"); }
+        }
+
+    }
+}
+
